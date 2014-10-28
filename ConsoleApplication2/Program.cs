8c52857adc1c50ef77020ee85047438fe6bfc046
matan86 <mauro.tangari@gmail.com>
2014-10-28 16:28:22 +0100

    test01

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -9,6 +9,23 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+        public static string GetStringFromDB(string key, string cultureCode)
+{
+if (SqlHelperClass.IsConnectionStringInitialized)
+{
+    QueryDataParameters queryParams = new QueryDataParameters();
+    queryParams.Add("@stringKey", key);
+    queryParams.Add("@cultureCode", cultureCode);
+    DataSet dataSource = SqlHelperClass.ExecuteQuery("Proc_CMS_SqlResourceManager_GetStringByKey", queryParams, QueryTypeEnum.StoredProcedure);
+    if (!SystemDataHelper.DataSourceIsEmpty(dataSource))
+    {
+        return dataSource.Tables[0].Rows[0]["TranslationText"].ToString();
+    }
+}
+return null;
+}
+
+
         }
     }
 }
